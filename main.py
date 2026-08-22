d7848b3892c31601c1ef15973bd3f02472703ee7
Pedro Henrique Martinez <henriquepedromartinez@gmail.com>
2026-08-22 04:17:49 -0300

    adição de novas funcionalidades do módulo loader e separação de embeddings

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from src.ingestion.loader import get_pdf_files
+from src.ingestion.loader import (
+    get_pdf_files,
+    load_pdf,
+    load_all_pdfs
+)
 
 
 def main():
@@ -7,15 +11,37 @@ def main():
         
         pdf_files = get_pdf_files("data/pdfs")
         
-        if not pdf_files:
-            print("Nenhum PDF encontrado")
-            return
-
+        first_pdf = pdf_files[0]
+        
+        documents = load_pdf(first_pdf)
+        
+        all_documents = load_all_pdfs("data/pdfs")
+        
+        # print(f"total de PDFs encontrados: {all_documents}")
         print(f"PDFs encontrados: {len(pdf_files)}")
+        print(f"total de Document carregados: {len(all_documents)}")
+                            
+        print("\nLista de documents\n")
 
-        for pdf in pdf_files:
-            print(pdf)
-
+        print(f"PDF: {first_pdf.name}")
+        print(f"Páginas carregadas: {len(documents)}")
+        
+        print("\nMETADATA\n")
+        print(documents[0].metadata)
+        
+        print("\nCONTEÚDO\n")
+        print(documents[0].page_content[:1000])
+        
+        print(type(documents))
+        print(type(documents[0])) 
+        
+        print(documents[0].metadata["page"])
+        print(documents[1].metadata["page"])
+        print(documents[2].metadata["page"])
+        
+        print(all_documents[0].metadata["source"])
+        print(all_documents[-1].metadata["source"])
+        
     except Exception as e:
         print(f"Erro:{e}")
 
--- src/ingestion/loader.py
+++ src/ingestion/loader.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from langchain_community.document_loaders import PyMuPDFLoader
 
 def get_pdf_files(pdf_directory: str) -> list[Path]:
     
@@ -16,3 +17,27 @@ def get_pdf_files(pdf_directory: str) -> list[Path]:
     pdf_files = list(directory.glob("*.pdf"))
     
     return pdf_files
+
+def load_pdf(pdf_path: Path):
+
+    loader = PyMuPDFLoader(str(pdf_path))
+
+    documents = loader.load()
+
+    return documents
+
+def load_all_pdfs(pdf_directory: str):
+    directory = Path(pdf_directory)
+    
+    pdf_files = list(directory.glob("*.pdf"))  
+    
+    all_documents = []
+
+    for pdf_path in pdf_files:
+        loader = PyMuPDFLoader(str(pdf_path))
+        
+        dados_pdf = loader.load()
+        
+        all_documents.extend(dados_pdf)
+        
+    return all_documents
