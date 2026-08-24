ee6a283b69542d11629dd49144b581b3407bddda
Pedro Henrique Martinez <henriquepedromartinez@gmail.com>
2026-08-24 02:17:08 -0300

    Executa teste completo de qualidade do retrieval

--- test_retrieval.py
+++ test_retrieval.py
@@ -4,10 +4,25 @@ results = retrieve(
     "Como funciona a comunicação entre microsserviços?"
 )
 
-print("IDS:")
-print(results["ids"])
-
-print("\n---------------------------------")
-
-print("DISTÂNCIAS:")
-print(results["distances"])
+for i in range(3):
+    
+    print(f"\nRESULTADO {i + 1}")
+    
+    print("-" * 50)
+    
+    print(
+        f"ID: {results['ids'][0][i]}"
+    )
+    
+    print(
+        f"DISTÂNCIA: {results['distances'][0][i]}"
+    )
+    
+    print("\nDOCUMENTO:\n")
+    
+    print(
+        results["documents"][0][i]
+    )
+    
+    print("\n")
+    
