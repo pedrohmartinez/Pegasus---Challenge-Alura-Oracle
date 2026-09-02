a132a1cc984654388f2c63f273331038a84f0c92
Pedro Henrique Martinez <henriquepedromartinez@gmail.com>
2026-09-01 23:50:36 -0300

    Adiciona lista das questions do benchmark de teste e cria laço de repetição para percorrer a lista

--- test_rag.py
+++ test_rag.py
@@ -1,31 +1,39 @@
 from src.rag.rag import answer_question
 from src.retrieval.retriever import retrieve
 
-question = (
-    "Explique como funciona o Windows."
-)
+# Lista das questions do benchmark de testes do RAG
+questions = [
+    "Qual é o principal produto da empresa?",
+    "Qual é a missão tecnológica da empresa?",
+    "Quantos funcionários a empresa possui?",
+    "Quais bancos de dados são utilizados?",
+    "Como os domínios se integram?",
+    "Quais mecanismos de segurança são utilizados na comunicação entre microsserviços?",
+    "Qual é o salário do CEO?",
+    "Qual é o faturamento anual da empresa?",
+    "Quem é o CEO da Santo Pegasus Soluciones?",
+    "Qual é o endereço físico da sede da empresa?",
+    "Qual é a capital do Brasil?",
+    "Explique como funciona o sistema operacional Windows."
+    ]
 
-answer = answer_question(
-    question
-)
-results = retrieve(question)
+for i, question in enumerate(questions):
+    answer = answer_question(
+        question
+    )
+    results = retrieve(question)
 
-print("\n================ RETRIEVAL ================\n")
+    print("\n================ RETRIEVAL ================\n")
 
-for result in results:
-    print("DOCUMENTO:")
-    print(result["document"])
+    for result in results:
+        print("DOCUMENTO:")
+        print(result["document"])
 
-    # print("\nMETADATA:")
-    # print(result["metadata"])
+        print("\nDISTANCE:")
+        print(result["distance"])
+        
+    print(f"\n================ RESPOSTA {i} ================\n")
 
-    print("\nDISTANCE:")
-    print(result["distance"])
+    print(answer)
 
     print("\n============================================\n")
-    
-print("\n================ RESPOSTA ================\n")
-
-print(answer)
-
-print("\n============================================\n")
